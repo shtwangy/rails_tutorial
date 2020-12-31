c61b4af8a2bf72788be00de4a27b62562308a8f1
shtwangy <shtwangy@gmail.com>
2021-01-01 01:31:40 +0900

    feat(app/models/weight_history.rb): add validation

--- app/models/weight_history.rb
+++ app/models/weight_history.rb
@@ -1,3 +1,15 @@
 class WeightHistory < ApplicationRecord
   belongs_to :user
+
+  # デフォルトで準備されているバリデーション（presence, length...）
+  validates :user_id, presence: {message: 'ユーザーを入力してください'}
+  validates :memo, length: {maximum: 255, too_long: 'メモは最大%{count}文字以内で入力してください'}
+
+  # カスタムバリデーション
+  validate :weight_validate
+  def weight_validate
+    if weight < 1
+      errors.add(:weight, '体重が正しくありません')
+    end
+  end
 end
