51e4422fe77fcb1989d2682ecf9aea2df6a30270
shtwangy <shtwangy@gmail.com>
2020-12-31 12:16:08 +0900

    feat(config/routes.rb): add users routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'top#index'
+  get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
 end
